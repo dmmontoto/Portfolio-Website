d7ba7a0e11c3e8290b0ec7ef90293e8bf1505b10
dmmontoto <dmontoto@ucsd.edu>
2023-08-09 13:44:20 -0700

    New drone animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,3 +36,61 @@ function stars() {
     }
 }
 stars();
+
+// $(document).ready(function() {
+//     var droneBox = $("#drone-box");
+//     var droneBackground = $("#drone-background");
+
+//     droneBackground.mousemove(function(e) {
+//         var droneBoxWidth = droneBackground.width();
+//         var droneBoxHeight = droneBackground.height();
+
+//         var droneBackgroundOffset = droneBackground.offset();
+//         var droneBackgroundWidth = droneBackground.width();
+//         var droneBackgroundHeight = droneBackground.height();
+
+//         var mouseX = e.pageX - droneBackgroundOffset.left;
+//         var mouseY = e.pageY - droneBackgroundOffset.top;
+
+//         var moveX = mouseX - droneBoxWidth / 2;
+//         var moveY = mouseY -50- droneBoxHeight / 2;
+
+//         moveX = Math.min(Math.max(moveX, -droneBoxWidth / 2), droneBackgroundWidth - droneBoxWidth / 2);
+//         moveY = Math.min(Math.max(moveY, -droneBoxHeight / 2), droneBackgroundHeight - droneBoxHeight / 2);
+
+//         droneBox.css({
+//             'transform': 'translate3d(' + moveX + 'px, ' + moveY + 'px, 0)'
+//         });
+//     });
+// });
+
+
+$(document).ready(function() {
+    var droneBox = $("#drone-box");
+    var droneBackground = $("#drone-background");
+
+    droneBackground.mousemove(function(e) {
+        var droneBoxWidth = droneBox.width();
+        var droneBoxHeight = droneBox.height();
+
+        var droneBackgroundOffset = droneBackground.offset();
+        var droneBackgroundWidth = droneBackground.width();
+        var droneBackgroundHeight = droneBackground.height();
+
+        var mouseX = e.pageX - droneBackgroundOffset.left;
+        var mouseY = e.pageY - droneBackgroundOffset.top;
+
+        var moveX = mouseX - 100 - droneBoxWidth / 2; // Adjusted x-axis shift
+        var moveY = mouseY - 200 - droneBoxHeight / 2;
+
+        // Adjust boundaries for x-axis
+        moveX = Math.min(Math.max(moveX, -50), droneBackgroundWidth - droneBoxWidth - 50);
+
+        // Adjust boundaries for y-axis
+        moveY = Math.min(Math.max(moveY, -droneBoxHeight / 2), droneBackgroundHeight - droneBoxHeight + droneBoxHeight / 2);
+
+        droneBox.css({
+            'transform': 'translate3d(' + moveX + 'px, ' + moveY + 'px, 0)'
+        });
+    });
+});
