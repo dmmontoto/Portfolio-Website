fceabe4248516925cb5dd2b2bbc1d284fb5404a9
dmmontoto <dmontoto@ucsd.edu>
2023-08-08 22:13:31 -0700

    Added my first home page animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,33 @@ $(document).ready(function() {
     });
 });
 
-document.addEventListener("scroll", function () {
-    var slideInElement = document.querySelector(".slide-in-element");
-    var elementPosition = slideInElement.getBoundingClientRect().top;
+// document.addEventListener("scroll", function () {
+//     var slideInElement = document.querySelector(".slide-in-element");
+//     var elementPosition = slideInElement.getBoundingClientRect().top;
 
-    if (elementPosition < window.innerHeight) {
-        slideInElement.classList.add("active");
+//     if (elementPosition < window.innerHeight) {
+//         slideInElement.classList.add("active");
+//     }
+// })
+
+function stars() {
+    let count = 50;
+    let scene = document.querySelector('.rocket-scene');
+    let i = 0;
+    while (i < count) {
+        let star = document.createElement('i');
+        let x = Math.floor(Math.random() * window.innerWidth);
+
+        let duration = Math.random() * 1;
+        let h = Math.random() * 100;
+
+        star.style.left = x + 'px';
+        star.style.width = 1 + 'px';
+        star.style.height = h + 'px';
+        star.style.animationDuration = duration + 's';
+
+        scene.appendChild(star);
+        i++;
     }
-})
+}
+stars();
