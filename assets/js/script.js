1a13aa509ca53279d091ed7cb2fcb1a1ff3e97da
dmmontoto <dmontoto@ucsd.edu>
2023-08-10 05:45:54 -0700

    Nasa API

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,7 @@ $(document).ready(function() {
     });
 });
 
-// document.addEventListener("scroll", function () {
-//     var slideInElement = document.querySelector(".slide-in-element");
-//     var elementPosition = slideInElement.getBoundingClientRect().top;
-
-//     if (elementPosition < window.innerHeight) {
-//         slideInElement.classList.add("active");
-//     }
-// })
-
+// Rocket
 function stars() {
     let count = 50;
     let scene = document.querySelector('.rocket-scene');
@@ -36,6 +28,30 @@ function stars() {
     }
 }
 stars();
+
+// Mars Rover
+function roverSearch(date, roverName, camera) {
+    const nasaApiKey = '2QbpLQBozt59EwMHuzZseMAHas7Z9Q6X2gVu7UFm';
+    
+    const apiUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?earth_date=${date}&camera=${camera}&api_key=${nasaApiKey}`;
+    
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            if (data.photos && data.photos.length > 0) {
+                const imageUrl = data.photos[0].img_src;
+                const imgElement = document.getElementById('img-display');
+                imgElement.src = imageUrl;
+            } else {
+                console.log('No image found for the provided criteria.');
+            }
+        })
+        .catch(error => {
+            console.error('An error occurred:', error);
+        });
+}
+roverSearch('2015-6-3', 'curiosity', 'FHAZ');
+
 
 // $(document).ready(function() {
 //     var droneBox = $("#drone-box");
@@ -65,6 +81,8 @@ stars();
 // });
 
 
+
+// Drone 
 $(document).ready(function() {
     var droneBox = $("#drone-box");
     var droneBackground = $("#drone-background");
