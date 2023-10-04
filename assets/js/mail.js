72bc6b48fd4d4c63e008458f94b20200cac47971
dmmontoto <dmontoto@ucsd.edu>
2023-10-03 22:06:50 -0700

    perfected contact

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,15 +1,26 @@
-function sendMail() {
-    var params = {
-        user_name : document.getElementById("user_name").value,
-        user_email : document.getElementById("user_email").value,
-        message : document.getElementById("message").value
-    }
-    emailjs.send("service_rxk2nr8", "template_0kv42on", params).then(function (res) {
-    })
-}
+const btn = document.getElementById('button');
+const user_name = document.getElementById('user_name');
+const user_email = document.getElementById('user_email');
+const description = document.getElementById('message');
 
-function thanks() {
-    var successMessage = document.getElementById("thanks");
-    successMessage.textContent = "Your message was received successfully! Thank you!";
-    return;
-}
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'service_rxk2nr8';
+   const templateID = 'template_0kv42on';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Email Sent!';
+      user_name.value = '';
+      user_email.value = '';
+      description.value = '';
+
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
