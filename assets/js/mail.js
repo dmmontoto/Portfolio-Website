3a8bf82cba41affce6f3ce7f06fa07f267ecdb99
dmmontoto <dmontoto@ucsd.edu>
2023-10-03 21:07:34 -0700

    adding thanks

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -6,7 +6,7 @@ function sendMail() {
         message : document.getElementById("message").value
     }
     emailjs.send("service_rxk2nr8", "template_0kv42on", params).then(function (res) {
-        thanks();
+        alert("Success!");
     })
 }
 
