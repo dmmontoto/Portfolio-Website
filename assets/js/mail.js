881242a875a28337b8a123f951cc9b4bebaf656a
dmmontoto <dmontoto@ucsd.edu>
2023-10-03 21:40:12 -0700

    small changes

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,19 +1,15 @@
 function sendMail() {
-    console.log("test");
     var params = {
         user_name : document.getElementById("user_name").value,
         user_email : document.getElementById("user_email").value,
         message : document.getElementById("message").value
     }
     emailjs.send("service_rxk2nr8", "template_0kv42on", params).then(function (res) {
-        alert("Success!");
     })
 }
 
 function thanks() {
-    var successMessage = document.createElement("p");
+    var successMessage = document.getElementById("thanks");
     successMessage.textContent = "Your message was received successfully! Thank you!";
-    
-    var button = document.getElementById("submit");
-    button.parentNode.insertBefore(successMessage, button);
+    return;
 }
