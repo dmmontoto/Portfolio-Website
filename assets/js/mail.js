064980ca4e573491a31cb3ed13cfa3854bdbdb2d
dmmontoto <dmontoto@ucsd.edu>
2023-10-03 21:03:06 -0700

    testing no gitignore

---
+++ assets/js/mail.js
@@ -0,0 +1,19 @@
+function sendMail() {
+    console.log("test");
+    var params = {
+        user_name : document.getElementById("user_name").value,
+        user_email : document.getElementById("user_email").value,
+        message : document.getElementById("message").value
+    }
+    emailjs.send("service_rxk2nr8", "template_0kv42on", params).then(function (res) {
+        thanks();
+    })
+}
+
+function thanks() {
+    var successMessage = document.createElement("p");
+    successMessage.textContent = "Your message was received successfully! Thank you!";
+    
+    var button = document.getElementById("submit");
+    button.parentNode.insertBefore(successMessage, button);
+}
